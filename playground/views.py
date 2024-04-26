ce822f2bfe3ec888f963e856c238ffbac57868fd
Behnam AslaniRokh <behnamaslani@proton.me>
2024-04-26 21:18:21 +0330

    Customers with .com accounts in exercise

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product, Customer
 
 
 def say_hello(request):
-    return HttpResponse('HELLO')
+    # queryset = Product.objects.filter(last_update__year=2021)
+    queryset = Customer.objects.filter(email__icontains='.com')
+    return render(request, 'hello.html', {'customers': queryset})
