750e690ab8ea30d8f6aedcf18fee2cacd7fd6b4c
Behnam AslaniRokh <behnamaslani@proton.me>
2024-04-26 21:24:39 +0330

    Collections that don’t have a featured product in exercise

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer
+from store.models import Product, Customer, Collection
 
 
 def say_hello(request):
     # queryset = Product.objects.filter(last_update__year=2021)
-    queryset = Customer.objects.filter(email__icontains='.com')
-    return render(request, 'hello.html', {'customers': queryset})
+    # queryset = Customer.objects.filter(email__icontains='.com')
+    queryset = Collection.objects.filter(featured_product__isnull=True)
+    return render(request, 'hello.html', {'collections': queryset})
