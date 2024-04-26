34ba2e7300332acc3d15a08947a6443106e7b37e
Behnam AslaniRokh <behnamaslani@proton.me>
2024-04-26 21:34:55 +0330

    Orders placed by customer with id = 1 in exercise

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer, Collection
+from store.models import Product, Customer, Collection, Order
 
 
 def say_hello(request):
     # queryset = Product.objects.filter(last_update__year=2021)
     # queryset = Customer.objects.filter(email__icontains='.com')
     # queryset = Collection.objects.filter(featured_product__isnull=True)
-    queryset = Product.objects.filter(inventory__lt=10)
-    return render(request, 'hello.html', {'products': queryset})
+    # queryset = Product.objects.filter(inventory__lt=10)
+    order = Order.objects.get(pk=1)
+    return render(request, 'hello.html', {'order': order})
