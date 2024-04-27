b27620cf0fccf5bfecd661265861a51699c3c535
Behnam AslaniRokh <behnamaslani@proton.me>
2024-04-27 20:57:01 +0330

    sorting data

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product, Customer, Collection, Order, OrderItems
+from django.db import connection
 
 
 def say_hello(request):
@@ -9,5 +10,12 @@ def say_hello(request):
     # queryset = Collection.objects.filter(featured_product__isnull=True)
     # queryset = Product.objects.filter(inventory__lt=10)
     # queryset = Order.objects.filter(customer_id=1)
-    queryset = OrderItems.objects.filter(product__collection__id=3)
-    return render(request, 'hello.html', {'ordersItems': queryset})
+    # queryset = OrderItems.objects.filter(product__collection__id=3)
+    # queryset = Product.objects.order_by('unit_price','-title').reverse()
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earliest('title')
+    product = Product.objects.latest('unit_price')
+    # print(queryset.query)
+
+
+    return render(request, 'hello.html', {'product': product})
